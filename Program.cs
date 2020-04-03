8ae91670e424edc7c31cce357f71013aed9b7391
Азиз Исматов <yourGitHub@example.co>
2020-04-03 23:46:04 +0500

    Создал класс и конструтор

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Student
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Student student = new Student("Anvar", "Hafizov","Abduloevich",21,"Tajik");
+            
+        }
+    }
+    class Student
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string middleName { get; set; }
+        public int  age { get; set; }  
+        public string nationality { get; set; }
+           
+    }
+}
